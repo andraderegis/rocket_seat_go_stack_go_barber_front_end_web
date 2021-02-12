4d4640ba9a46bc30881079005960e8d95911cd6f
andraderegis <reginaldo.andrade@outlook.com>
2021-02-12 15:12:59 -0300

    Extract input and button components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
-import GlobalStyle from './styles/global';
+import GlobalStyle from 'styles/global';
 
-import SingIn from './pages/SignIn';
+import SingIn from 'pages/SignIn';
+
+// import GlobalStyle from './styles/global';
+
+// import SingIn from './pages/SignIn';
 
 const App: React.FC = () => (
   <>
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  align-items: center;
+  display: flex;
+
+  background: #232129;
+  border: 2px solid #232129;
+  border-radius: 10px;
+  color: #666360;
+  padding: 16px;
+  width: 100%;
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  input {
+    background: transparent;
+    border: 0;
+    flex: 1;
+    color: #f4ede8;
+
+    &::placeholder {
+      color: #666360;
+    }
+  }
+
+  svg {
+    margin-right: 16px;
+  }
+`;
